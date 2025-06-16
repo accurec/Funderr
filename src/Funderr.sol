26f31efd29c78549d4c58fa521670a9d8f3077c0
accurec <max.zhernovkov@gmail.com>
2025-06-16 16:19:05 -0700

    Remved todo item

--- src/Funderr.sol
+++ src/Funderr.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 // TODO: Consolidate all errors/reverts into modifiers
-// TODO: Add small fee to start a campaign for profitability and also prevent campaign spam attacks
 // TODO: Add contributed to campaigns mapping
 // TODO: Add campaign URL (?)
 // TODO: Move errors into separate file
@@ -57,13 +56,30 @@ contract Funderr {
 
     // Events
 
-    event CampaignCreated(uint256 indexed campaignId, address indexed owner, uint256 goal, uint256 deadline);
+    event CampaignCreated(
+        uint256 indexed campaignId,
+        address indexed owner,
+        uint256 goal,
+        uint256 deadline
+    );
 
-    event ContributionReceived(uint256 indexed campaignId, address indexed contributor, uint256 amount);
+    event ContributionReceived(
+        uint256 indexed campaignId,
+        address indexed contributor,
+        uint256 amount
+    );
 
-    event FundsWithdrawn(uint256 indexed campaignId, address indexed owner, uint256 amount);
+    event FundsWithdrawn(
+        uint256 indexed campaignId,
+        address indexed owner,
+        uint256 amount
+    );
 
-    event RefundIssued(uint256 indexed campaignId, address indexed contributor, uint256 amount);
+    event RefundIssued(
+        uint256 indexed campaignId,
+        address indexed contributor,
+        uint256 amount
+    );
 
     event FeesCollected(address indexed collector, uint256 amount);
 
@@ -164,14 +180,18 @@ contract Funderr {
         uint256 feesToTransfer = feesCollected;
         feesCollected = 0;
 
-        (bool callSuccess,) = payable(msg.sender).call{value: feesToTransfer}("");
+        (bool callSuccess, ) = payable(msg.sender).call{value: feesToTransfer}(
+            ""
+        );
 
         if (!callSuccess) revert Funderr__CollectFeesCallFailed();
 
         emit FeesCollected(msg.sender, feesToTransfer);
     }
 
-    function contribute(uint256 campaignId) external payable campaignExists(campaignId) afterDeadline(campaignId) {
+    function contribute(
+        uint256 campaignId
+    ) external payable campaignExists(campaignId) afterDeadline(campaignId) {
         if (msg.value == 0) revert Funderr__ContributionMustBeGreaterThanZero();
 
         Campaign storage c = campaigns[campaignId];
@@ -182,11 +202,9 @@ contract Funderr {
         emit ContributionReceived(campaignId, msg.sender, msg.value);
     }
 
-    function withdrawCampaignContributions(uint256 campaignId)
-        external
-        onlyCampaignOwner(campaignId)
-        beforeDeadline(campaignId)
-    {
+    function withdrawCampaignContributions(
+        uint256 campaignId
+    ) external onlyCampaignOwner(campaignId) beforeDeadline(campaignId) {
         Campaign storage c = campaigns[campaignId];
 
         if (c.totalContributed == 0) {
@@ -199,18 +217,18 @@ contract Funderr {
 
         c.fundsWithdrawn = true;
 
-        (bool callSuccess,) = payable(msg.sender).call{value: c.totalContributed}("");
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: c.totalContributed
+        }("");
 
         if (!callSuccess) revert Funderr__WithdrawContributionsCallFailed();
 
         emit FundsWithdrawn(campaignId, c.owner, c.totalContributed);
     }
 
-    function refundContributorContributions(uint256 campaignId)
-        external
-        campaignExists(campaignId)
-        beforeDeadline(campaignId)
-    {
+    function refundContributorContributions(
+        uint256 campaignId
+    ) external campaignExists(campaignId) beforeDeadline(campaignId) {
         Campaign storage c = campaigns[campaignId];
 
         if (c.contributions[msg.sender] == 0) revert Funderr__NothingToRefund();
@@ -219,14 +237,19 @@ contract Funderr {
             revert Funderr__CampaignContributionsHasBeenWithdrawn();
         }
 
-        if (c.totalContributed >= c.goal && block.timestamp <= c.deadline + i_activeFundedCampaignWindow) {
+        if (
+            c.totalContributed >= c.goal &&
+            block.timestamp <= c.deadline + i_activeFundedCampaignWindow
+        ) {
             revert Funderr__CampaignGoalHasBeenReachedAndWithdrawPeriodHasNotExpired();
         }
 
         uint256 amountToRefund = c.contributions[msg.sender];
         c.contributions[msg.sender] = 0;
 
-        (bool callSuccess,) = payable(msg.sender).call{value: amountToRefund}("");
+        (bool callSuccess, ) = payable(msg.sender).call{value: amountToRefund}(
+            ""
+        );
 
         if (!callSuccess) revert Funderr__RefundContributionCallFailed();
 
@@ -263,27 +286,47 @@ contract Funderr {
         return i_createCampaignFee;
     }
 
-    function getCampaign(uint256 campaignId)
+    function getCampaign(
+        uint256 campaignId
+    )
         external
         view
         campaignExists(campaignId)
-        returns (uint256, address, uint256, uint256, uint256, bool, string memory, string memory)
+        returns (
+            uint256,
+            address,
+            uint256,
+            uint256,
+            uint256,
+            bool,
+            string memory,
+            string memory
+        )
     {
         Campaign storage c = campaigns[campaignId];
 
-        return (campaignId, c.owner, c.goal, c.deadline, c.totalContributed, c.fundsWithdrawn, c.title, c.description);
+        return (
+            campaignId,
+            c.owner,
+            c.goal,
+            c.deadline,
+            c.totalContributed,
+            c.fundsWithdrawn,
+            c.title,
+            c.description
+        );
     }
 
-    function getCampaingContributionByContributor(uint256 campaignId, address contributor)
-        external
-        view
-        campaignExists(campaignId)
-        returns (uint256)
-    {
+    function getCampaingContributionByContributor(
+        uint256 campaignId,
+        address contributor
+    ) external view campaignExists(campaignId) returns (uint256) {
         return campaigns[campaignId].contributions[contributor];
     }
 
-    function getCampaignsByOwner(address owner) external view returns (uint256[] memory) {
+    function getCampaignsByOwner(
+        address owner
+    ) external view returns (uint256[] memory) {
         return campaignsByOwner[owner];
     }
 
